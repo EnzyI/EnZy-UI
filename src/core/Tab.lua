3e53e56ff7109061409a029ae5cf99c6507667e4
EnzyI <zyxrywsp@gmail.com>
2026-01-04 10:18:56 +0700

    Update Tab.lua

--- src/core/Tab.lua
+++ src/core/Tab.lua
@@ -1,30 +1,49 @@
 local Tab = {}
 Tab.__index = Tab
 
-local Button = require(script.Parent.Parent.components.Button)
-local Toggle = require(script.Parent.Parent.components.Toggle)
+local ButtonComponent = require(script.Parent.Parent.components.Button)
+local ToggleComponent = require(script.Parent.Parent.components.Toggle)
+local SliderComponent = require(script.Parent.Parent.components.Slider)
 
-function Tab.new(Parent, name)
+function Tab.new(Window, name)
     local self = setmetatable({}, Tab)
+
+    local TabButton = Instance.new("TextButton")
+    TabButton.Size = UDim2.new(1,-10,0,36)
+    TabButton.BackgroundTransparency = 1
+    TabButton.Text = name
+    TabButton.TextColor3 = Color3.fromRGB(160,160,160)
+    TabButton.Parent = Window.TabsBar
 
     local Container = Instance.new("Frame")
     Container.Size = UDim2.fromScale(1,1)
     Container.BackgroundTransparency = 1
-    Container.Parent = Parent
+    Container.Visible = false
+    Container.Parent = Window.TabsHolder
 
     local Layout = Instance.new("UIListLayout", Container)
     Layout.Padding = UDim.new(0,8)
 
+    TabButton.MouseButton1Click:Connect(function()
+        Window:SwitchTab(self)
+    end)
+
+    self.Button = TabButton
     self.Container = Container
+
     return self
 end
 
 function Tab:CreateButton(cfg)
-    return Button.new(self.Container, cfg)
+    return ButtonComponent.new(self.Container, cfg)
 end
 
 function Tab:CreateToggle(cfg)
-    return Toggle.new(self.Container, cfg)
+    return ToggleComponent.new(self.Container, cfg)
+end
+
+function Tab:CreateSlider(cfg)
+    return SliderComponent.new(self.Container, cfg)
 end
 
 return Tab
